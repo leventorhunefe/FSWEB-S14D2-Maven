c836519b33b27e67c2c9188309cdcae684c70942
Levent Orhun Efe <orhunefe93@gmail.com>
2024-06-10 00:20:15 +0300

    final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+
+import org.example.model.Bed;
+import org.example.model.Ceiling;
+import org.example.model.Lamp;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lamp lamp = new Lamp(LampType.NEON, true, 100);
+        Ceiling ceiling = new Ceiling(100, PaintColor.YELLOW);
+        Bed bed = new Bed("double", 2, 3, 2, 3);
     }
 }
