f4069a80511039c06c869bcbcd4a5be3eddb1a5e
Levent Orhun Efe <orhunefe93@gmail.com>
2024-06-11 21:40:35 +0300

    final

---
+++ src/main/java/org/example/model/Bed.java
@@ -0,0 +1,54 @@
+package org.example.model;
+
+public class Bed {
+    private String style;
+    private int pillows;
+    private int height;
+    private int sheets;
+    private int quilts;
+
+    public Bed(String style, int pillows, int height, int sheets, int quilt) {
+        this.style = style;
+        this.pillows = pillows;
+        this.height = height;
+        this.sheets = sheets;
+        this.quilts = quilts;
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+
+    public int getQuilts() {
+        return quilts;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void make(){
+        System.out.println("The bed is being made.");
+    }
+
+    @Override
+    public String toString() {
+        return "Bed{" +
+                "style='" + style + '\'' +
+                ", pillows=" + pillows +
+                ", height=" + height +
+                ", sheets=" + sheets +
+                ", quilts=" + quilts +
+                '}';
+    }
+
+}
